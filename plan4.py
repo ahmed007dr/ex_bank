0f989edc8d0ca4ed98b5be7359fc270c0620a9d9
ahmed007dr <ahmed007eg@gmail.com>
2023-11-18 11:27:43 +0200

    part new

--- plan4.py
+++ plan4.py
@@ -1,5 +1,16 @@
 class bank :
     def __init__(self,name,age) -> None:
+        
         print(f'welcome {name}')
+        self.blance=0
+    def deposit (self,amount):
+        self.blance += amount
+        print(self.blance)
+
 
 c1=bank('ahmed',12)
+c1.deposit(500)
+c1.deposit(1000)
+
+
+
