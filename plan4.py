6cbeffd156f3a1d203f472eba3b2b4ccadc1b258
ahmed007dr <ahmed007eg@gmail.com>
2023-11-18 11:54:46 +0200

    full details

--- plan4.py
+++ plan4.py
@@ -1,8 +1,10 @@
 class bank :
     def __init__(self,name,age) -> None:
-        
         print(f'welcome {name}')
         self.blance=0
+        self.name = name
+        self.age=age
+
     def deposit (self,amount):
         self.blance += amount
         print(self.blance)
@@ -14,12 +16,16 @@ class bank :
         self.blance -=amout
         print(self.blance)
 
+    def show_Details(self):
+        print(f'Name {self.name}')
+        print(f'age {self.age}')
+        print(f'my blance {self.blance}')
 
 
 c1=bank('ahmed',12)
 c1.deposit(500)
 c1.deposit(1000)
 c1.withdrow(33)
-
+c1.show_Details()
 
 
